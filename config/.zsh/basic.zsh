d1711feb5b903c75754a9a04aea78fe8cd5ab974
igsr5 <i@gsr5.dev>
2022-04-19 00:33:57 +0900

    Add .zsh/basic.zsh

---
+++ config/.zsh/basic.zsh
@@ -0,0 +1,22 @@
+# env
+setopt IGNOREEOF
+
+# cd
+setopt auto_cd
+chpwd() { ls -l -a }
+
+# hitory
+HISTFILE=~/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt share_history
+
+# directory stack
+setopt auto_pushd
+setopt pushd_ignore_dups
+DIRSTACKSIZE=100
+
+
