a91238508b4bafc760b95517897d64764e7624c4
igsr5 <i@gsr5.dev>
2022-04-19 01:39:14 +0900

    cookbooks/homebrew

---
+++ cookbooks/homebrew/default.rb
@@ -0,0 +1,4 @@
+execute 'Install Homebrew' do
+  command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)> >/dev/null'
+  not_if "test $(which brew)"
+end
