49b298406bdc4434476577afb93631b64eaf2b98
igsr5 <i@gsr5.dev>
2022-04-21 01:54:30 +0900

    Add alias for ubuntu ec2

--- config/.zsh/aliases.zsh
+++ config/.zsh/aliases.zsh
@@ -5,3 +5,18 @@ alias re='source ~/.zshrc'
 alias mkdir='mkdir -p'
 alias ls='ls -l -a'
 alias valec='AWS_SDK_LOAD_CONFIG=1 valec'
+
+alias ec2-ubuntu-start="
+  aws ec2 start-instances --instance-ids i-045d4a6f5bf5315ed \
+  && aws ec2 wait instance-running --instance-ids i-045d4a6f5bf5315ed \
+  && aws ec2 describe-instances --instance-ids i-045d4a6f5bf5315ed \
+  | jq '.Reservations[].Instances[] | \
+  {InstanceId, InstanceState: .State.Name, PublicDnsName, PublicIpAddress}'
+"
+
+alias ec2-ubuntu-stop="
+  aws ec2 stop-instances --instance-ids i-045d4a6f5bf5315ed \
+  && aws ec2 wait instance-stopped --instance-ids  i-045d4a6f5bf5315ed \
+  && aws ec2 describe-instances --instance-ids i-045d4a6f5bf5315ed \
+  | jq '.Reservations[].Instances[] | {InstanceId, Name, InstanceState: .State}'
+  "
