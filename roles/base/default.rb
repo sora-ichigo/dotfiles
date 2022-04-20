1c420224efe0da1495f8fbbef43d0a62f45b7a08
igsr5 <i@gsr5.dev>
2022-04-20 12:21:40 +0900

    Update base/default.rb

--- roles/base/default.rb
+++ roles/base/default.rb
@@ -1,8 +1,11 @@
 include_cookbook 'functions'
 
+# first
+include_cookbook 'zsh'
+
+# second
 include_cookbook 'git'
 include_cookbook 'go'
-include_cookbook 'zsh'
 include_cookbook 'tmux'
 include_cookbook 'vim'
 include_cookbook 'fzf'
