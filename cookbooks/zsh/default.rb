0cb4c2dff47a09e8de878d00371d7d15341737fa
igsr5 <i@gsr5.dev>
2022-04-20 15:58:11 +0900

    Update

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -1,10 +1,7 @@
 package 'zsh'
 
 if node[:platform] != 'darwin'
-  dotfile '.zshrc.Linux'
-
   execute "chsh -s /bin/zsh #{node[:user]}" do
-    only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
     user 'root'
   end
 end
