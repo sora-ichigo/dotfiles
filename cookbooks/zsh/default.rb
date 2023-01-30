f8ff8935eb05d513475347464336d8aabcaa2724
igsr5 <igsr5.dev@gmail.com>
2023-01-30 10:53:10 +0900

    Update

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -1,8 +1,8 @@
 package 'zsh'
 
-if ENV["SHELL"] != "bin/zsh"
-  execute "chsh -s /bin/zsh #{node[:user]}"
-end
+# if ENV["SHELL"] != "bin/zsh"
+#   execute "chsh -s /bin/zsh #{node[:user]}"
+# end
 
 dotfile '.zsh'
 dotfile '.zshrc'
