fa07cad26d80e8c5d19916da3303d99f2461575c
igsr5 <igsr5.dev@gmail.com>
2024-02-02 08:59:16 +0900

    Refactor vscode cookbook to fix extension installation

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -4,10 +4,10 @@ dotfile "Library/Application Support/Cursor/User/extensions"
 
 execute 'Install extensions' do
   command `
-cat $HOME/Library/Application Support/Cursor/User/extensions | while read line
-do
-code --install-extension $line
-done
-  `
+    cat $HOME/Library/Application\ Support/Cursor/User/extensions | while read line
+    do
+      code --install-extension $line
+    done
+    `
 end
 
