6c2ed8246dd398ba423258f86429fd6f9f185368
igsr5 <igsr5.dev@gmail.com>
2024-02-02 08:56:43 +0900

    Add list-extensions command to Makefile and install extensions in vscode

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ mitamae:
 	./bin/mitamae local ./lib/recipe.rb
 mitamae-dry:
 	./bin/mitamae local -n ./lib/recipe.rb
+list-extensions:
+	cursor --list-extensions > ./config/Library/Application\ Support/Cursor/User/extensions
--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -1,2 +1,13 @@
 dotfile "Library/Application Support/Cursor/User/settings.json"
 dotfile "Library/Application Support/Cursor/User/keybindings.json"
+dotfile "Library/Application Support/Cursor/User/extensions"
+
+execute 'Install extensions' do
+  command `
+cat $HOME/Library/Application Support/Cursor/User/extensions | while read line
+do
+code --install-extension $line
+done
+  `
+end
+
