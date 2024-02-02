32cc34034c31a9b4f0e7317cf0b2ce46f94cfd98
igsr5 <igsr5.dev@gmail.com>
2024-02-02 09:12:41 +0900

    Add ms-vscode.makefile-tools extension and update vscode installation command

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -3,11 +3,11 @@ dotfile "Library/Application Support/Cursor/User/keybindings.json"
 dotfile "Library/Application Support/Cursor/User/extensions"
 
 execute 'Install extensions' do
-  command `
-    cat $HOME/Library/Application\ Support/Cursor/User/extensions | while read line
+  command "
+    cat $HOME/Library/Application\\ Support/Cursor/User/extensions | while read line
     do
-      code --install-extension $line
+      cursor --install-extension $line
     done
-    `
+    "
 end
 
