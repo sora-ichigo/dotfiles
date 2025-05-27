4ad92c6908dc318aa2096aea59fd906b7c6d0d95
Sora ichigo <igsr5.dev@gmail.com>
2025-05-27 12:49:40 +0900

    Fix

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -1,6 +1,16 @@
 define :dotfile, source: nil do
   source = params[:source] || params[:name]
-  link File.join(ENV['HOME'], params[:name]) do
+  target_path = File.join(ENV['HOME'], params[:name])
+  target_dir = File.dirname(target_path)
+
+  # リンク先のディレクトリが存在しない場合は作成する
+  execute "mkdir -p #{target_dir}" do
+    command "mkdir -p #{target_dir}"
+    user node[:user]
+    not_if "test -d #{target_dir}"
+  end
+
+  link target_path do
     to File.expand_path("../../../config/#{source}", __FILE__)
     user node[:user]
     force true
