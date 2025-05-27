0c7e54e4a119b4045cac3c39bb431be7ce42d984
Sora ichigo <igsr5.dev@gmail.com>
2025-05-27 14:34:43 +0900

    Update yabai/default.rb

--- cookbooks/yabai/default.rb
+++ cookbooks/yabai/default.rb
@@ -1,5 +1,20 @@
-package 'yabai'
-package 'skhd'
+execute "add yabai tap" do
+  command "brew tap koekeishiya/formulae"
+  not_if "brew tap | grep koekeishiya/formulae"
+  user node[:user] if node[:platform] == 'darwin'
+end
+
+execute "install yabai" do
+  command "brew install koekeishiya/formulae/yabai"
+  not_if "which yabai >/dev/null 2>&1"
+  user node[:user] if node[:platform] == 'darwin'
+end
+
+execute "install skhd" do
+  command "brew install koekeishiya/formulae/skhd"
+  not_if "which skhd >/dev/null 2>&1"
+  user node[:user] if node[:platform] == 'darwin'
+end
 
 dotfile '.yabairc'
 dotfile '.skhdrc'
