f22af15f70dd0489c872ed7ef49b925720253ea2
Sora ichigo <igsr5.dev@gmail.com>
2026-01-17 13:30:34 +0900

    feat: zsh basic.zsh に便利な設定を追加する

--- config/.zsh/basic.zsh
+++ config/.zsh/basic.zsh
@@ -1,11 +1,14 @@
 # env
 setopt IGNOREEOF
+setopt no_beep
+setopt interactive_comments
+setopt glob_dots
 
 # cd
 setopt auto_cd
 chpwd() { ls -l -a }
 
-# hitory
+# history
 HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
@@ -13,6 +16,8 @@ setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt hist_reduce_blanks
 setopt share_history
+setopt inc_append_history
+setopt extended_history
 
 # directory stack
 setopt auto_pushd
@@ -27,4 +32,8 @@ compinit -u
 autoload -Uz colors
 colors
 autoload -U +X bashcompinit && bashcompinit
+
+# completion
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' menu select
 
