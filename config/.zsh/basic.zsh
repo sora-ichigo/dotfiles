b107ce8e30533f4fc58e4311e909831da24e30a4
Sora ichigo <igsr5.dev@gmail.com>
2026-03-14 14:58:38 +0900

    feat: zsh の履歴検索で重複を除外する設定を追加する

--- config/.zsh/basic.zsh
+++ config/.zsh/basic.zsh
@@ -13,6 +13,7 @@ HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
 setopt hist_ignore_all_dups
+setopt hist_find_no_dups
 setopt hist_ignore_space
 setopt hist_reduce_blanks
 setopt share_history
