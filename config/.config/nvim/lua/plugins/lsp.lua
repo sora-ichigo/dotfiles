fb097b12ef52d8acf004b372059db50f5844259f
Sora ichigo <igsr5.dev@gmail.com>
2026-06-30 10:02:58 +0900

    fix: Mason 非対応の dbt LSP 設定を削除して起動エラーを解消する

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -12,16 +12,6 @@ return {
         root_dir = util.root_pattern("package.json", "tsconfig.json"),
         single_file_support = false,
       })
-      opts.servers.dbt = {
-        cmd = { "dbt-language-server", "--stdio" },
-        filetypes = { "sql" },
-        root_dir = util.root_pattern("dbt_project.yml"),
-        single_file_support = false,
-      }
     end,
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = { ensure_installed = { "dbt-language-server" } },
   },
 }
