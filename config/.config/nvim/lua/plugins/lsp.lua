ce713f977fd51ee69842151b7cf25c8ca1829ca6
Sora ichigo <igsr5.dev@gmail.com>
2026-06-30 10:08:13 +0900

    fix: root_dir を nvim 0.11 の新シグネチャに修正して LSP がアタッチしない問題を解消する

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -2,14 +2,28 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      local util = require("lspconfig.util")
+      local deno_markers = { "deno.json", "deno.jsonc" }
+      local node_markers = { "package.json", "tsconfig.json" }
 
       opts.servers = opts.servers or {}
       opts.servers.denols = {
-        root_dir = util.root_pattern("deno.json", "deno.jsonc"),
+        root_dir = function(bufnr, on_dir)
+          local root = vim.fs.root(bufnr, deno_markers)
+          if root then
+            on_dir(root)
+          end
+        end,
       }
       opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, {
-        root_dir = util.root_pattern("package.json", "tsconfig.json"),
+        root_dir = function(bufnr, on_dir)
+          if vim.fs.root(bufnr, deno_markers) then
+            return
+          end
+          local root = vim.fs.root(bufnr, node_markers)
+          if root then
+            on_dir(root)
+          end
+        end,
         single_file_support = false,
       })
     end,
