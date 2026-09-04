a2f66a0bc90bb565c039bd5a304cae7f4cf49484
Sora ichigo <igsr5.dev@gmail.com>
2026-09-04 13:40:08 +0900

    fix: 「垂直タブを閉じる」の割り当てを option+ctrl+b に変更する

--- nix/programs/chrome.nix
+++ nix/programs/chrome.nix
@@ -3,7 +3,7 @@
 {
   config = lib.mkIf pkgs.stdenv.isDarwin {
     targets.darwin.defaults."com.google.Chrome".NSUserKeyEquivalents = {
-      "垂直タブを閉じる" = "^b";
+      "垂直タブを閉じる" = "~^b";
     };
   };
 }
